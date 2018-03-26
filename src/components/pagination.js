4205450341ec1ce9857cebd1e4afb0e80f444715
coiorz <coiorz1989@gmail.com>
2018-03-26 12:44:03 +0800

    pagination styles

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin: .5rem 0;
+`;
 
 const PageBtn = styled.span`
  display: inline-block;
- padding: 1rem;
- border: 1px solid black;
+ padding: 0 .2rem;
+ margin: .3rem;
+ cursor: pointer;
+ border-bottom: 1px solid transparent;
+ ${p => p.active && css`
+    color: #4c7af1;
+    font-weight: 700;
+    border-bottom: 1px solid #4c7af1;
+ `}
 `;
 
 export default class Pagination extends Component {
+  state = {
+    currentIndex: 0,
+  }
+
   render() {
     const {
       length = 1,
-      onClick,
     } = this.props;
+
+    const { currentIndex } = this.state;
 
     return (
       <Container>
         {[...Array(length).keys()].map(k => (
-          <PageBtn key={k} onClick={onClick.bind(this, k)}>{k + 1}</PageBtn>
+          <PageBtn 
+            key={k} 
+            active={k === currentIndex}
+            onClick={this.clickPage.bind(this, k)}
+          >
+            {k + 1}
+          </PageBtn>
         ))}
       </Container>
     );
+  }
+
+  clickPage = k => {
+    this.setState(() => ({
+      currentIndex: k,
+    }));
+    this.props.onClick(k);
   }
 }
