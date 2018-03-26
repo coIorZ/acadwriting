c7a9667aedf0c2ec9d78f46f5718d322eed62fa1
Eileen <linshan002@e.ntu.edu.sg>
2018-03-26 11:25:23 +0800

    add pagination to sentences

--- src/modules/home/components/right-panel/sentences.js
+++ src/modules/home/components/right-panel/sentences.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 
+import Pagination from '../../../../components/pagination';
+
+const COUNT_PER_PAGE = 10;
+
 const Container = styled.div``;
 
+const Sentence = styled.div`
+  margin: 1rem 0;
+`;
+
 export default class Sentences extends Component {
+  state = {
+    currentIndex: 0,
+  }
+
   render() {
     const {
       sentences,
@@ -12,13 +24,24 @@ export default class Sentences extends Component {
 
     const sentencesByMarkerId = sentences[markerId];
 
-    return sentencesByMarkerId ? (
+    if(!sentencesByMarkerId) return <div>loading sentences...</div>;
+    console.log(Object.keys(sentencesByMarkerId).length);
+    const { currentIndex } = this.state;
+
+    return (
       <Container>
-        {Object.keys(sentencesByMarkerId).map(sentenceId => {
+        {Object.keys(sentencesByMarkerId).slice(currentIndex * COUNT_PER_PAGE, (currentIndex + 1) * COUNT_PER_PAGE).map(sentenceId => {
           const sentence = sentencesByMarkerId[sentenceId];
-          return <div>{sentence.text}</div>;
+          return <Sentence>{sentence.text}</Sentence>;
         })}
+        <Pagination length={(Object.keys(sentencesByMarkerId).length / COUNT_PER_PAGE | 0) + 1} onClick={this.turnPage}/>
       </Container>
-    ) : <div>loading sentences...</div>;
+    );
+  }
+
+  turnPage = index => {
+    this.setState(() => ({
+      currentIndex: index,
+    }));
   }
 }
