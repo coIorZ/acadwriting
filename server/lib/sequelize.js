700c7b42635fcc702c9c0980eb11a63f447f4618
coiorz <coiorz1989@gmail.com>
2018-04-03 19:34:06 +0800

    modify db password

--- server/lib/sequelize.js
+++ server/lib/sequelize.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-export default new Sequelize('acadwrit_infobase', 'root', 'toor', {
+export default new Sequelize('acadwrit_infobase', 'root', '', {
   host             : 'localhost',
   dialect          : 'mysql',
   operatorsAliases : false,
