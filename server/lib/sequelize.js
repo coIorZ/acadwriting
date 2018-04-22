771c6bb6d60cfb51c780bb43a0ee0a637d7cb8f9
coiorz <coiorz1989@gmail.com>
2018-04-23 02:13:08 +0800

    change db config

--- server/lib/sequelize.js
+++ server/lib/sequelize.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 
-export default new Sequelize('acadwrit_infobase', 'root', '', {
+export default new Sequelize('acadwrit_infobase', 'root', '123', {
   host             : 'localhost',
   dialect          : 'mysql',
   operatorsAliases : false,
+  insecureAuth     : true,
   define           : {
     timestamps: false,
   },
--- server/models/md-sub-codes.js
+++ server/models/md-sub-codes.js
@@ -5,7 +5,7 @@ import sequelize from '../lib/sequelize';
 export default sequelize.define('Metadiscourse_Function_Subcategories', {
   id       : { type: Sequelize.INTEGER, field: 'metad_funct_subcat_id', primaryKey: true },
   mdCodeId : { type: Sequelize.INTEGER, field: 'metad_funct_id' },
-  label    : { type: Sequelize.STRING, field: 'display' },
+  label    : { type: Sequelize.STRING, field: 'metad_funct_subcat_code' },
   desc     : { type: Sequelize.STRING, field: 'metad_funct_subcat_description' },
 }, {
   freezeTableName: true,
